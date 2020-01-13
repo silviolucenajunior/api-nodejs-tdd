5e842727f798feae5ee3c85b2bb14d05cf13de4f
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2020-01-13 15:35:31 +1300

    [CC] - Move router code to a class

--- login.js
+++ login.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose')
 const AccountModel = mongoose.Model('Account')
 
 module.exports = () => {
-    router.post('singup/', async(request, response) => {
+    router.post('singup/', new SingupRouter().route)
+}
+
+class SingupRouter {
+    async route(request, response) {
         const { email, password, repeatPassword } = request.body
         if (password === repeatPassword) {
             const client = await AccountModel.create({ email, password })
             return response.json(client)
         }
         response.status(400).json({ error: 'Passwords don\'t match' })
-    })
+    }
 }
